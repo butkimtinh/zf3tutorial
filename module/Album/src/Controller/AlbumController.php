bfda40f377f3336df87c53934794ab2035a42b13
Joe Vu <joe@onlinebizsoft.com>
2017-09-19 10:52:18 +0700

    Routing and controllers

---
+++ module/Album/src/Controller/AlbumController.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * OnlineBiz Software Solution
+ * 
+ * @version 0.0.1
+ * @author Joe Vu<joe@onlinebizsoft.com>
+ * @see http://onlinebizsoft.com
+ * @copyright (c) 2017 , OnlineBiz Software Solution
+ * 
+ * Create at: Sep 19, 2017 10:35:07 AM
+ */
+
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController {
+
+    public function indexAction() {
+        
+    }
+
+    public function addAction() {
+        
+    }
+
+    public function editAction() {
+        
+    }
+
+    public function deleteAction() {
+        
+    }
+
+}
